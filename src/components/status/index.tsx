0428735f8d389178e69c064a76a7ac8ac0a243e0
Matheus Hora <matheus11hora@gmail.com>
2023-06-30 08:36:30 -0300

    situação finalizado

--- src/components/status/index.tsx
+++ src/components/status/index.tsx
@@ -1,14 +1,51 @@
-import styles from "./status.module.css";
-import Image from "next/image";
-import icon from "../../assets/icons/Assignment.svg";
+import React from "react";
 
-export default function Status() {
+interface Status {
+  active: boolean;
+}
+
+const Status = (props: Status) => {
+  const statusStyle = {
+    backgroundColor: props.active ? "#E9F8E7" : "#FFE6E6",
+    color: props.active ? "#1EB106" : "#FF0101",
+    display: "inline-flex",
+    alignItems: "center",
+    padding: "2px 8px",
+    borderRadius: "16px",
+  };
+
+  const iconName = props.active ? "check" : "close";
+
   return (
-    <div className={styles.container}>
-      <i className={styles.icon}>
-        <Image width={10} height={10} alt="" src={icon} />
-      </i>
-      <p className={styles.text}>Inativo</p>
+    <div style={statusStyle}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        width="20"
+        height="20"
+        style={{ marginRight: "4px" }}
+      >
+        {props.active ? (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M5 13l4 4L19 7"
+          />
+        ) : (
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M6 18L18 6M6 6l12 12"
+          />
+        )}
+      </svg>
+      {props.active ? "Ativo" : "Inativo"}
     </div>
   );
-}
+};
+
+export default Status;
