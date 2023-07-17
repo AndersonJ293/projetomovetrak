97d6a3177aa8146a8f0fc3c498798d2207fa9509
Gustavo Francisco <ciscosweater@gmail.com>
2023-07-17 13:42:44 -0300

    Modais de contato e editar relatório

--- src/components/modal/contacts/index.tsx
+++ src/components/modal/contacts/index.tsx
@@ -1,54 +1,70 @@
 import Image from "next/image";
 import svg from "../../../../public/icons/phone.svg";
 import React from "react";
-import PropTypes from "prop-types";
 import styles from "./modal.module.css";
+import CloseIcon from "../../../../public/icons/close.svg";
 
-const ModalContacts = ({ title, listItems }) => {
+interface ModalContactsProps {
+  title: string
+  listItems?: any
+}
+
+const listItemsMock = [
+  {
+    name: "John Doe",
+    email: "johndoe@example.com",
+    phone: "123-456-7890",
+  },
+  {
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    phone: "987-654-3210",
+  },
+  {
+    name: "Bob Johnson",
+    email: "bobjohnson@example.com",
+    phone: "555-555-5555",
+  },
+];
+
+const ModalContacts = (props: ModalContactsProps) => {
   return (
-    <div className={styles.modal}>
-      <div className={styles.modalHeader}>
-        <h2>{title}</h2>
-        <button className={styles.closeButton} onClick={undefined}>
-          Fechar
-        </button>
-      </div>
-      <div className={styles.lists}>
-        <div className={styles.listItem}>
-          {listItems.map((item, index) => (
-            <div key={index} className={styles.listItemContent}>
-              <div className={styles.iconTwo}>
-                <Image
-                  src={svg}
-                  width={18}
-                  height={18}
-                  alt={"icone nota"}
-                  className={styles.iconSvg}
-                ></Image>
+    <div className={styles.modalBackground}>
+      <div className={styles.modal}>
+        <div className={styles.modalHeader}>
+          <h2 className={styles.title}>{props.title}</h2>
+          <div
+            className={styles.closeButton}
+            // onClick={() => props.fechar(false)}
+          >
+            <Image src={CloseIcon} width={25} height={25} alt="" />
+          </div>
+        </div>
+        <div className={styles.lists}>
+          <div className={styles.listItem}>
+            {listItemsMock.map((item, index) => (
+              <div key={index} className={styles.listItemContent}>
+                <div className={styles.iconTwo}>
+                  <Image
+                    src={svg}
+                    width={18}
+                    height={18}
+                    alt={"icone nota"}
+                    className={styles.iconSvg}
+                  ></Image>
+                </div>
+                <div>
+                  <p className={styles.itemName}>{item.name}</p>
+                  <p className={styles.itemSubtitle}>{item.email}</p>
+                  <p className={styles.itemSubtitle}>{item.phone}</p>
+                </div>
               </div>
-              <div>
-                <p className={styles.itemName}>{item.name}</p>
-                <p className={styles.itemSubtitle}>{item.email}</p>
-                <p className={styles.itemSubtitle}>{item.phone}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
-};
-
-ModalContacts.propTypes = {
-  title: PropTypes.string.isRequired,
-  // onClose: PropTypes.func.isRequired,
-  listItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-      phone: PropTypes.string.isRequired,
-    })
-  ).isRequired,
 };
 
 export default ModalContacts;
--- src/components/modal/reportEdit/index.tsx
+++ src/components/modal/reportEdit/index.tsx
@@ -1,88 +1,86 @@
 import Image from "next/image";
-import svg from "../../../assets/icons/note.svg";
+import svg from "../../../../public/icons/note.svg";
 import React from "react";
-import PropTypes from "prop-types";
 import styles from "./modal.module.css";
+import CloseIcon from "../../../../public/icons/close.svg";
 
-const Modal = ({
-  title,
-  onClose,
-  listItems,
-  subtitle,
-  otherLists,
-  label,
-  value,
-  buttonText,
-}) => {
+interface ReportEditModalProps {
+  title: string
+  onClose?: any
+  listItems?: any
+  subtitle: string
+  otherLists?: any
+  label: string
+  value?: any
+  buttonText: string
+}
+
+const listItemsMock = [
+  {
+    name: "Anderson Junior",
+    subtitle: "Relatório Semanal Prefeitura",
+  }
+];
+
+const otherListsMock = [
+  {
+    title: "Validação de Motor Ocioso",
+    subtitle: "Tensão da Bateria",
+  },
+  {
+    title: "Tensão da Bateria",
+    subtitle: "13.4 V",
+  },
+];
+
+const ReportEditModal = (props: ReportEditModalProps) => {
   return (
-    <div className={styles.modal}>
-      <div className={styles.modalHeader}>
-        <h2>{title}</h2>
-        <button className={styles.closeButton} onClick={undefined}>
-          Fechar
-        </button>
-      </div>
-      <div className={styles.lists}>
-        <div className={styles.listItem}>
-          {listItems.map((item, index) => (
-            <div key={index} className={styles.listItemContent}>
-              <div className={styles.iconTwo}>
-                <Image
-                  src={svg}
-                  width={18}
-                  height={18}
-                  alt={"icone nota"}
-                  className={styles.iconSvg}
-                ></Image>
+    <div className={styles.modalBackground}>
+      <div className={styles.modal}>
+        <div className={styles.modalHeader}>
+          <h2 className={styles.title}>{props.title}</h2>
+          <div
+            className={styles.closeButton}
+            // onClick={() => props.fechar(false)}
+          >
+            <Image src={CloseIcon} width={25} height={25} alt="" />
+          </div>
+        </div>
+        <div className={styles.lists}>
+          <div className={styles.listItem}>
+            {listItemsMock.map((item, index) => (
+              <div key={index} className={styles.listItemContent}>
+                <div className={styles.iconTwo}>
+                  <Image
+                    src={svg}
+                    width={18}
+                    height={18}
+                    alt={"icone nota"}
+                    className={styles.iconSvg}
+                  ></Image>
+                </div>
+                <div>
+                  <p className={styles.itemName}>{item.name}</p>
+                  <p className={styles.itemSubtitle}>{item.subtitle}</p>
+                </div>
               </div>
-              <div>
-                <p className={styles.itemName}>{item.name}</p>
-                <p className={styles.itemSubtitle}>{item.subtitle}</p>
-              </div>
+            ))}
+            <div className={styles.time}>23/06/23 ás 16:15</div>
+          </div>
+          <div className={styles.subtitle}>{props.subtitle}</div>
+          {otherListsMock.map((list, index) => (
+            <div key={index} className={styles.otherList}>
+              <h3 className={styles.otherListTitle}>{list.title}</h3>
+              <p className={styles.otherListSubtitle}>{list.subtitle}</p>
             </div>
           ))}
-          <div className={styles.time}>23/06/23 ás 16:15</div>
         </div>
-        <div className={styles.subtitle}>{subtitle}</div>
-        {otherLists.map((list, index) => (
-          <div key={index} className={styles.otherList}>
-            <h3 className={styles.otherListTitle}>{list.title}</h3>
-            <p className={styles.otherListSubtitle}>{list.subtitle}</p>
-          </div>
-        ))}
-      </div>
-      <label>{label}</label>
-      <input className={styles.input} value={value} />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-        }}
-      >
-        <button className={styles.button}>{buttonText}</button>
+        <label className={styles.label}>{props.label}</label>
+        <input className={styles.input} value={props.value} />
+        <button className={styles.button}>{props.buttonText}</button>
       </div>
     </div>
   );
 };
 
-Modal.propTypes = {
-  title: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired,
-  listItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      subtitle: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  subtitle: PropTypes.string.isRequired,
-  otherLists: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      subtitle: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  value: PropTypes.string.isRequired,
-  buttonText: PropTypes.string.isRequired,
-};
-
-export default Modal;
+export default ReportEditModal;
